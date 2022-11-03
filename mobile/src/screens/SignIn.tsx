3818a42c2a782bac4c33145c54454df41bb4909f
Bruno Becoski <brunobecoski@gmail.com>
2022-11-03 17:02:43 -0300

    Mobile: finalizando a interface de SignIn

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Center, Icon } from 'native-base'
+import { Center, Icon, Text } from 'native-base'
 import { Fontisto } from '@expo/vector-icons'
 
 import { Button } from '../components/Button'
@@ -7,14 +7,24 @@ import Logo from '../assets/logo.svg'
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
+    <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
 
       <Button
-        title="ENTRAR COM GOOGLE"
+        mt={12}
         type="SECONDARY"
+        title="ENTRAR COM GOOGLE"
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
       />
+
+      <Text
+        mt={4}
+        color="gray.200"
+        textAlign="center"
+      >
+        Não utilizamos nenhuma informação além  {'\n'}
+        do seu e-mail para criação de sua conta.
+      </Text>
     </Center>
   )
 }
