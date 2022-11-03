f99145aead86307636898085d3f08eba5a869131
Bruno Becoski <brunobecoski@gmail.com>
2022-11-03 17:23:56 -0300

    Mobile: criando o contexto

---
+++ mobile/src/contexts/AuthContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, ReactNode } from 'react'
+
+interface UserProps {
+  name: string;
+  avatarUrl: string;
+}
+
+export interface AuthContextDataProps {
+  user: UserProps;
+  signIn: () => Promise<void>;
+}
+
+interface AuthContextProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextDataProps)
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  
+  async function signIn() {
+    console.log('Vanmos logar!')
+  }
+
+  return (
+    <AuthContext.Provider value={{
+      signIn,
+      user: {
+        name: 'Bruno',
+        avatarUrl: 'https://github.com/brunobecoski.png',
+      }
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
