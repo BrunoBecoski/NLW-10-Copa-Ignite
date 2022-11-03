4fcdb24708a2da257e925237e36a914aa052b776
Bruno Becoski <brunobecoski@gmail.com>
2022-11-03 16:31:47 -0300

    Mobile: criando o componente de Button

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,4 +1,7 @@
-import { Center } from 'native-base'
+import { Center, Icon } from 'native-base'
+import { Fontisto } from '@expo/vector-icons'
+
+import { Button } from '../components/Button'
 
 import Logo from '../assets/logo.svg'
 
@@ -6,6 +9,12 @@ export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900">
       <Logo width={212} height={40} />
+
+      <Button
+        title="ENTRAR COM GOOGLE"
+        type="SECONDARY"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+      />
     </Center>
   )
 }
