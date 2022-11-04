cf56365f9163f17421392e17be9251246a5e8e2d
Bruno Becoski <brunobecoski@gmail.com>
2022-11-04 18:12:52 -0300

    Server: criando a rota /me

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,10 +2,16 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 
 import { prisma } from '../lib/prisma'
+import { authenticate } from '../plugins/authenticate'
 
 export async function authRoutes(fastify: FastifyInstance) {
-  fastify.post('/users',async (request) => {
+  fastify.get('/me', {
+    onRequest: [authenticate],
+  }, async (request) => {
+    return { user: request.user }
+  })
 
+  fastify.post('/users',async (request) => {
     const createUserBody = z.object({
       access_token: z.string(),
     })
@@ -47,6 +53,14 @@ export async function authRoutes(fastify: FastifyInstance) {
       })
     }
 
-    return { userInfo }
+    const token = fastify.jwt.sign({
+      name: user.name,
+      avatarUrl: user.avatarUrl,
+    }, {
+      sub: user.id,
+      expiresIn: '7 days',
+    })
+
+    return { token }
   })
 }
